7846cab8c6987756cce1844e7b6344436893a9db
piro0526 <piro0526@gmail.com>
2023-11-13 02:08:53 +0900

    attr

--- src/tic_tac_toe.cpp
+++ src/tic_tac_toe.cpp
@@ -19,15 +19,16 @@ PYBIND11_MODULE(tic_tac_toe, m)
         .def("getMark", &Disc::getMark)
         .def("setMark", &Disc::getMark)
         ;
-    
-    m.attr("BOARD_ROW_SIZE") = BOARD_ROW_SIZE;
-    m.attr("BOARD_COL_SIZE") = BOARD_COL_SIZE;
-    m.attr("K_IN_A_ROW") = K_IN_A_ROW;
-    m.attr("MAX_TURNS") = MAX_TURNS;
-    m.attr("EMPTY") = 0;
-    m.attr("X") = -1;
-    m.attr("O") = 1;
-    m.attr("WALL") = 2;
+
+    m.attr("BOARD_ROW_SIZE") = py::cast(BOARD_ROW_SIZE);
+    m.attr("BOARD_COL_SIZE") = py::cast(BOARD_COL_SIZE);
+    m.attr("K_IN_A_ROW") = py::cast(K_IN_A_ROW);
+    m.attr("MAX_TURNS") = py::cast(MAX_TURNS);
+
+    m.attr("EMPTY") = py::cast(0);
+    m.attr("X") = py::cast(-1);
+    m.attr("O") = py::cast(1);
+    m.attr("WALL") = py::cast(2);
 
 
 
@@ -39,7 +40,7 @@ PYBIND11_MODULE(tic_tac_toe, m)
         .def("checkLine", &Board::checkLine)
         .def("isGameOver", &Board::isGameOver)
         .def("switchMark", &Board::switchMark)
-        .def("getMark", &Board::getMark)
+        .def("getMark", &Board::getMark, "get Mark")
         .def("getPlaceablePosition", &Board::getPlaceablePosition)
         .def("getUpdate", &Board::getUpdate)
         .def("getCurrentMark", &Board::getCurrentMark)
